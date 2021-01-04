a125babe37d19cf3f486f51bc757e82e625ccdb6
Jeff MacDonald <jam@zoidtechnologies.com>
2021-01-03 20:15:34 -0500

    casino/blackjack/: initial commit.

---
+++ blackjack/blackjack.py
@@ -0,0 +1,18 @@
+import argparse
+
+import ttyio4 as ttyio
+import bbsengine4 as bbsengine
+
+def main():
+    parser = argparse.ArgumentParser("blackjack")
+    
+    parser.add_argument("--verbose", action="store_true", dest="verbose")
+    parser.add_argument("--debug", action="store_true", dest="debug")
+
+    defaults = {"databasename": "zoidweb4", "databasehost":"localhost", "databaseuser": None, "databaseport":5432, "databasepassword":None}
+    bbsengine.buildargdatabasegroup(parser, defaults)
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
