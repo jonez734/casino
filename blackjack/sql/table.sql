350073ed03e8e81c2c9797ce70fbeee6f2b7e22c
Jeff MacDonald <jam@zoidtechnologies.com>
2021-07-27 18:14:44 -0400

    casino/blackjack/sql/table.sql: added attributes 'minimumbet', 'maximumbet', 'bank', and 'casinoid'

--- blackjack/sql/table.sql
+++ blackjack/sql/table.sql
@@ -1,4 +1,8 @@
 create or replace view casino.table as
     select
-      *
+      *,
+      (attributes->>'minimumbet')::bigint as minimumbet,
+      (attributes->>'maximumbet')::bigint as meximumbet,
+      (attributes->>'bank')::bigint as bank,
+      (attributes->>'casinoid')::bigint as casinoid
     from engine.node where prg='casino.table';
