2ac660643c5a8852a3179f533e84fddfc76d0c3a
Jeff MacDonald <jam@zoidtechnologies.com>
2022-04-04 21:41:00 -0400

    - casino/sql/game.sql: removed table casino.__cardgame and view casino.cardgame - updated casio.game

--- sql/game.sql
+++ sql/game.sql
@@ -1,16 +1,26 @@
 \echo game
-create table if not exists casino.__cardgame (
-    "id" bigserial unique not null primary key,
-    "datestarted" timestamptz,
-    "minplayer" bigint default 1,
-    "maxplayer" bigint default 2,
-    "status" text,
-    "type" text not null
-);
+--create table if not exists casino.__game (
+--    "id" bigserial unique not null primary key,
+--    "datestarted" timestamptz,
+--    "minplayer" bigint default 1,
+--    "maxplayer" bigint default 2,
+--    "status" text,
+--    "type" text not null
+--);
 
-create or replace view casino.cardgame as
-    select
-      *,
-      extract(epoch from datestarted) as datestartedepoch
-    from casino.__cardgame
+--create or replace view casino.game as
+--    select
+--      g.*,
+--      extract(epoch from g.datestarted) as datestartedepoch
+--    from casino.__game as g
+--;
+
+create or replace view casino.game as
+  select
+    *,
+    (attributes->>'casinoid')::bigint as casinoid,
+    (attributes->>'handid')::bigint as handid
+    (attributes->>'playerid')::bigint as playerid
+  from engine.node
+  where prg='casino.game'
 ;
