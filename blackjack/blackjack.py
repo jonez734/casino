d68fee9dca623b7f4b15df4680caa4c13e22394d
Jeff MacDonald <jam@zoidtechnologies.com>
2022-04-08 18:49:37 -0400

    - new player and dealer globals - add attributes 'currentbet' and 'hand' to Player - add sethand() to Player - in play(), replace dealerhand with dealer.hand - in play(), replace playerhand with player.hand - in play(), show actual hand status instead of 'player wins' or 'dealer wins' (shows natural blackjack, etc) - dealer.hand and player.hand are lists - in main(), create 2 instances of Player: player and dealer

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -6,6 +6,9 @@ import bbsengine5 as bbsengine
 
 import casino
 
+player = None
+dealer = None
+
 class Hand:
     def __init__(self, label="NEEDINFO"):
         self.playerid = None
@@ -61,35 +64,45 @@ class Hand:
 
         ttyio.echo(" [%d]" % (self.calcvalue()), level="debug")
 
-def Player():
+class Player(object):
     def __init__(self, memberid=None, playerid=None):
         self.memberid = memberid
         self.id = playerid
+        self.currentbet = 0
+        self.hand = None
+
+    def sethand(self, hand=None):
+        self.hand = hand
 
 def play(shoe, dealerhand, playerhand):
+    global player, dealer
+
     choice = None
+
+    dealer.sethand(dealerhand)
+    player.sethand(playerhand)
 
     done = False
     while not done:
-        playerhand.show()
-        dealerhand.show()
+        player.hand.show()
+        dealer.hand.show()
 
-        playerstatus = playerhand.status()
+        playerstatus = player.hand.status()
         ttyio.echo("player status: %s" % (playerstatus))
         if playerstatus == "win" or playerstatus == "naturalblackjack" or playerstatus == "blackjack":
-            ttyio.echo("player wins")
-            ttyio.echo("dealer lose")
+            ttyio.echo("player wins: %s" % (playerstatus))
+            ttyio.echo("dealer loss")
             break
         if playerstatus == "bust":
             ttyio.echo("player bust")
             ttyio.echo("dealer win")
             break
 
-        dealerstatus = dealerhand.status()
+        dealerstatus = dealer.hand.status()
         ttyio.echo("dealer status: %s" % (dealerstatus))
         if dealerstatus == "win" or dealerstatus == "naturalblackjack" or dealerstatus == "blackjack":
-            ttyio.echo("dealer wins")
-            ttyio.echo("player lose")
+            ttyio.echo("dealer wins: %s" % (dealerstatus))
+            ttyio.echo("player loss")
             break
         if dealerstatus == "bust":
             ttyio.echo("dealer bust")
@@ -101,24 +114,24 @@ def play(shoe, dealerhand, playerhand):
                 ttyio.echo("stand")
                 choice = "stand"
             elif ch == "H":
-                playerhand.append(shoe.draw())
+                player.hand.append(shoe.draw())
                 ttyio.echo("hit")
                 choice = "hit"
 
-        playervalue = playerhand.calcvalue()
+        playervalue = player.hand.calcvalue()
         # playervalue = playerhand.adjustace()
 
-        dealervalue = dealerhand.calcvalue()
+        dealervalue = dealer.hand.calcvalue()
         # dealervalue = dealerhand.adjustace()
 
-        playerhand.show()
-        dealerhand.show()
+        player.hand.show()
+        dealer.hand.show()
 
         if playervalue > 21:
             break
 
         # dealer
-        dealervalue = dealerhand.calcvalue()
+        dealervalue = dealer.hand.calcvalue()
         # dealervalue = dealerhand.adjustace()
 
         if dealervalue > 21:
@@ -129,12 +142,12 @@ def play(shoe, dealerhand, playerhand):
             break
         if dealervalue == 17:
             ttyio.echo("dealer hits on soft 17", level="debug")
-            dealerhand.append(shoe.draw(), hidden=True)
+            dealer.hand.append(shoe.draw(), hidden=True)
         elif dealervalue < 17:
             ttyio.echo("dealer < 17, hit", level="debug")
-            dealerhand.append(shoe.draw())
+            dealer.hand.append(shoe.draw())
 
-        dealervalue = dealerhand.calcvalue()
+        dealervalue = dealer.hand.calcvalue()
 
         if dealervalue > 21:
             break
@@ -145,8 +158,8 @@ def play(shoe, dealerhand, playerhand):
             continue
 
     ttyio.echo("end hand")
-    dealerhandvalue = dealerhand.calcvalue()
-    playerhandvalue = playerhand.calcvalue()
+    dealerhandvalue = dealer.hand.calcvalue()
+    playerhandvalue = player.hand.calcvalue()
     if dealerhandvalue > 21:
         playerstatus = "win"
         dealerstatus = "bust"
@@ -162,9 +175,23 @@ def play(shoe, dealerhand, playerhand):
     elif playerhandvalue > dealerhandvalue:
         playerstatus = "win"
         dealerstatus = "loss"
+    elif dealerhandvalue == 21:
+        if len(dealer.hand) == 2:
+            dealerstatus = "naturalblackjack"
+            playerstatus = "loss"
+        else:
+            dealerstatus = "blackjack"
+            playerstatus = "loss"
+    elif playerhandvalue == 21:
+        if len(player.hand) == 2:
+            playerstatus = "naturalblackjack"
+            dealerstatus = "loss"
+        else:
+            playerstatus = "blackjack"
+            dealerstatus = "loss"
 
-    playerhand.show(hide=False)
-    dealerhand.show(hide=False)
+    player.hand.show(hide=False)
+    dealer.hand.show(hide=False)
 
     if dealerstatus in ("win", "naturalblackjack", "blackjack"):
         ttyio.echo("dealer: %s, player: %s" % (dealerstatus, playerstatus))
@@ -174,6 +201,8 @@ def play(shoe, dealerhand, playerhand):
         ttyio.echo("push")
 
 def main():
+    global player, dealer
+
     parser = argparse.ArgumentParser("blackjack")
     
     parser.add_argument("--verbose", action="store_true", dest="verbose")
@@ -192,16 +221,23 @@ def main():
 #    shoe.show()
 #    ttyio.echo("shuffled shoe=%r" % (shoe))
 
+    player = Player()
+    ttyio.echo("player=%r" % (player), level="debug")
+    dealer = Player()
+
     done = False
     while not done:
         playerhand = Hand("player")
+        player.sethand(playerhand)
+
         dealerhand = Hand("dealer")
+        dealer.sethand(dealerhand)
+
+        player.hand.append(shoe.draw())
+        dealer.hand.append(shoe.draw())
 
         playerhand.append(shoe.draw())
-        dealerhand.append(shoe.draw())
-
-        playerhand.append(shoe.draw())
-        dealerhand.append(shoe.draw())
+        dealer.hand.append(shoe.draw())
 
         play(shoe, dealerhand, playerhand)
         if ttyio.inputboolean("another hand? [Yn]: ", "Y") is False:
