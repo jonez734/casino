73bfc6a368f10fd41992d92c65ac0ad41326a43a
Jeff MacDonald <jam@zoidtechnologies.com>
2024-05-07 20:23:26 -0400

    - casino/main.py: added.

---
+++ src/casino/main.py
@@ -0,0 +1,14 @@
+from bbsengine6 import util
+
+def init(args, **kw:dict) -> bool:
+    return True
+
+def access(args, op:str, **kw:dict) -> bool:
+    return True
+
+def buildargs(args, **kw:dict) -> bool:
+    return None
+
+def main(args, **kw):
+    util.heading("casino")
+    return True
