8f7d04e3f68a57808f1be5d3a3858b222a94c5ed
J (eff) <jam@zoidtechnologies.com>
2026-06-21 23:57:39 -0400

    Add accountid column to __table with FK to bank.__account
    - Removed unused bank column
    - Added accountid bigint with FK constraint to bank.__account(id)

--- src/casino/sql/table.sql
+++ src/casino/sql/table.sql
@@ -1,8 +1,20 @@
-create or replace view casino.table as
-    select
-      *,
-      (attributes->>'minimumbet')::bigint as minimumbet,
-      (attributes->>'maximumbet')::bigint as meximumbet,
-      (attributes->>'bank')::bigint as bank,
-      (attributes->>'casinoid')::bigint as casinoid
-    from engine.node where prg='casino.table';
+create table if not exists casino.__table (
+    "moniker" citext unique not null primary key,
+    "type" text,
+    "minimumbet" numeric(10,0) default 100,
+    "maximumbet" numeric(10,0) default 100,
+    "location" text,
+    "ownermoniker" citext constraint fk_table_ownermoniker references engine.__member(moniker) on update cascade on delete set null,
+    "ownersince" timestamptz,
+    "accountid" bigint constraint fk_table_accountid references bank.__account(id) on delete set null,
+    "earnings" numeric(10,0),
+    "cheat" boolean default False,
+    "cheatpercent" integer,
+    "attrs" jsonb,
+    "shoe_cards" text[] default null,
+    "shoe_uses" integer default 0,
+    "status" text default 'open'
+);
+
+grant select on casino.__table to web;
+grant all on casino.__table to term, sysop;
