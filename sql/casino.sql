8c6a1411c89c3616dc68569641f0682e038461f1
J (eff) <jam@zoidtechnologies.com>
2026-06-22 23:24:26 -0400

    Remove legacy sql/ directory

--- sql/casino.sql
+++
@@ -1,5 +0,0 @@
-\i schema.sql
-\i player.sql
-\i cardtable.sql
-\i game.sql
---\i betlog.sql
--- sql/mapgameplayer.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists casino.mapgameplayer (
-    "gameid" bigint constraint fk_casino_map_gameid references casino.__game(id) on update cascade on delete cascade,
-    "playerid" bigint constraint fk_casino_map_playerid references casino.__player(id) on update cascade on delete cascade
-);
-
-grant all on casino.mapgameplayer to apache;
