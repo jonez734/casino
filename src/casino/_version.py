8a1901b1a940d5d9cc9b83af5acbe1d2cc3199bd
J (eff) <jam@zoidtechnologies.com>
2026-08-05 20:34:42 -0400

    casino: stamp _version.py

--- src/casino/_version.py
+++ src/casino/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202607221856"
-githash = "c920564537c3551e"
-datestamp = "202607221856"
+__version__ = "0.0.1.dev202608052034"
+githash = "3eb574cece846c8c"
+datestamp = "202608052034"
