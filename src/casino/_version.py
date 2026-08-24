84559a5724d7ae94953c7638023c6b169702aa46
J (eff) <jam@zoidtechnologies.com>
2026-08-24 09:35:45 -0400

    chore(casino): bump _version.py to 202608240912

--- src/casino/_version.py
+++ src/casino/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608222038"
-githash = "01d50ce"
-datestamp = "202608222038"
+__version__ = "0.0.1.dev202608240912"
+githash = "6408dcf1a5a1795c"
+datestamp = "202608240912"
